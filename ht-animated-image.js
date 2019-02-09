2891963f188e1d91e4d935d5c1cf3d99adc4667a
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2019-02-09 11:50:34 +0300

    new lit-element changes

--- ht-animated-image.js
+++ ht-animated-image.js
@@ -1,6 +1,42 @@
 "use strict";
-import { LitElement, html } from "@polymer/lit-element";
+import { LitElement, html, css } from "lit-element";
+
 class HTAnimatedImage extends LitElement {
+  static styles = css`<style>
+    :host {
+      display: block;
+      position:relative;
+      box-sizing:border-box;
+    }
+
+    video {
+      display:block;
+    }
+    
+    #label {
+      border-radius: 3px;
+      opacity: 1;
+      line-height: 20px;
+      padding: 0 8px;
+      font-size: 9px;
+      font-weight: 700;
+      color: #fff;
+      background-color: rgba(0, 0, 0, .3);
+      position: absolute;
+      transition: opacity 150ms ease;
+      bottom: 16px;
+      left: 16px;
+    }
+    
+    #container:hover #label {
+      display:none;
+    }
+
+    [hidden] {
+      display:none;
+    }
+  </style>`;
+
   render() {
     const { data, loop } = this;
     if (data === undefined || data.public_id === undefined) return;
@@ -14,52 +50,14 @@ class HTAnimatedImage extends LitElement {
       data.version
     }/${data.public_id}.webm`;
     return html`
-      <style>
-        :host {
-          display: block;
-          position:relative;
-          box-sizing:border-box;
-        }
-
-        video {
-          display:block;
-        }
-        
-        #label {
-          border-radius: 3px;
-          opacity: 1;
-          line-height: 20px;
-          padding: 0 8px;
-          font-size: 9px;
-          font-weight: 700;
-          color: #fff;
-          background-color: rgba(0, 0, 0, .3);
-          position: absolute;
-          transition: opacity 150ms ease;
-          bottom: 16px;
-          left: 16px;
-        }
-        
-        #container:hover #label {
-          display:none;
-        }
-
-        [hidden] {
-          display:none;
-        }
-      </style>
       <div id="container">
-        <video width="100%" height="auto" ?autoplay=${loop} loop muted="muted" poster=${poster}>
-            <source type="video/mp4" src=${mp4}>
-            <source type="video/webm" src=${webm}>
+        <video width="100%" height="auto" ?autoplay="${loop}" loop muted="muted" poster="${poster}">
+            <source type="video/mp4" src="${mp4}">
+            <source type="video/webm" src="${webm}">
         </video>
-        <div id="label" ?hidden=${loop}>GIF</div>
+        <div id="label" ?hidden="${loop}">GIF</div>
       </div>
 `;
-  }
-
-  static get is() {
-    return "ht-animated-image";
   }
 
   static get properties() {
@@ -87,4 +85,4 @@ class HTAnimatedImage extends LitElement {
   }
 }
 
-customElements.define(HTAnimatedImage.is, HTAnimatedImage);
+customElements.define("ht-animated-image", HTAnimatedImage);
